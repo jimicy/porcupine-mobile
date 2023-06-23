6684ce2e76045f60bcc9be1881c35d70d487fc6f
Jimmy Wang <jimmy.wang@figma.com>
2023-06-23 01:52:31 -0700

    Add book appointment page

---
+++ lib/Screens/GetTested/health_care_provider.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HealthCareProvider extends StatelessWidget {
+  const HealthCareProvider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+      child: ListView(padding: const EdgeInsets.all(8), children: <Widget>[
+        Text(
+          "Radiant Health Centers",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        Text("17982 Sky Park Circle, Suite J"),
+        Text("Irvine, CA 92614"),
+        Row(
+          children: [
+            Icon(Icons.phone),
+            Text("714-454-6955"),
+          ],
+        ),
+        Divider(
+          thickness: 2,
+        ),
+        Text("Free Service"),
+        Text("• Hepatitis C Test"),
+        Text("• HIV Self-Testing"),
+        Text("• HIV Test"),
+        Text("• STD Test"),
+        Divider(
+          thickness: 2,
+        ),
+        Text("On-site Service"),
+        Text("• Chlamydia Test"),
+        Text("• Conventional HIV Test"),
+        Text("• Gonorrhea Test"),
+        Text("• Hepatitis B Test"),
+        Text("• Hepatitis C Test"),
+        Text("• Herpes Test"),
+        Text("• Rapid HIV Test"),
+        Text("• Syphilis Test"),
+        Text("Mail-only self-test kits"),
+        Text("• HIV Self-Testing"),
+        SizedBox(height: 50),
+        Center(
+          child: ElevatedButton(
+            // style: style,
+            onPressed: () {},
+            child: const Text('Book Appointment'),
+          ),
+        )
+      ]),
+    );
+  }
+}
